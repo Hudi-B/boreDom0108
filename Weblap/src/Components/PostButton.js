14c95187c5601ddc8987c19e9a4223d517d8d683
RRichard2004 <ricsiruszo90@gmail.com>
2023-12-25 00:25:10 +0100

    new post modifications
    styleing modified
    input image done,
    multiple line content
    dynamic height post popup

--- Weblap/src/Components/PostButton.js
+++ Weblap/src/Components/PostButton.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Popup from 'reactjs-popup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faFeather, faTimes, faList, faCat, faPaintBrush, faRobot, faPersonWalking, faUtensils, faBookAtlas, faLeaf, faMicrochip, faCarSide, faQuestion } from '@fortawesome/free-solid-svg-icons';
+import defaultImage from '../defaultimage.jpg';
 
 export default function PostButton( {coll} ) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(defaultImage); // Set the default image URL
   const [category, setCategory] = useState('');
   const [icon, setIcon] = useState(faQuestion);
+
+  const fileInput = useRef(null);
+  const handleImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+  
+      reader.onloadend = (e) => {
+        setImageUrl(reader.result);
+      };
+  
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
 
   const valtoztat = (newIcon) => {
     setIcon(newIcon);
@@ -35,7 +50,7 @@ export default function PostButton( {coll} ) {
       image,
       category,
     };
-  if(title.trim() === "" || content.trim() === "" || category === "" || image === "") {
+  if(title.trim() === "" || content.trim() === "" || category === "" || image === null) {
     alert("Please fill in all the fields")
   } else {
                           console.log(postData) //testing
@@ -48,34 +63,69 @@ export default function PostButton( {coll} ) {
     } catch (error) {
       console.error(error);
     } 
+  }
+
+
+
 };
 
-
-
-}
   return (
     <Popup trigger={<button className={`postButton ${coll ? 'collapsed' : ''}`}>{coll ? <FontAwesomeIcon icon={faFeather}/> : <div className='buttontext'>Post</div>}</button>} modal nested>
       {(close) => (
         <div className='bigBox' onClick={close}>
           <div className='inputBox' onClick={(event) => event.stopPropagation()} style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
             <FontAwesomeIcon onClick={close} className='closeIcon' icon={faTimes} />
-            <form onSubmit={handleSubmit}>
-              <label>Title:</label>
-              <input type="text" name='Title' onChange={(e) => setTitle(e.target.value)} />
-              <label>Content:</label>
-              <input type="text" name='Content' onChange={(e) => setContent(e.target.value)} />
-              <label>Image:</label>
-              <input type="text" name='Image' onChange={(e) => setImage(e.target.value)} />
+            <form className='form' onSubmit={handleSubmit}>
+
+            <div>
+      <input
+        type='file'
+        accept='image/*'
+        ref={fileInput}
+        style={{ display: 'none' }}
+        onChange={handleImageChange}
+      />
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt='Selected'
+          style={{ maxWidth: '50%', height: 'auto' }}
+          onClick={() => fileInput.current.click()}
+        />
+      )}
+    </div>
+              <div className='szoveg'>
+                <input 
+                  type="text" 
+                  placeholder='Name your post' 
+                  onFocus={(e) => e.target.placeholder=''} 
+                  onBlur={(e) => e.target.placeholder='Name your post'}
+                  name='Title' 
+                  id='Title' 
+                  onChange={(e) => setTitle(e.target.value)} 
+                  required
+              />
+                <textarea 
+                  placeholder='Your content goes here...' 
+                  onFocus={(e) => e.target.placeholder=''} 
+                  onBlur={(e) => e.target.placeholder='Your content goes here...'}
+                  name='Content' 
+                  id='Content' 
+                  onChange={(e) => setContent(e.target.value)} 
+                  required
+                />
+              </div>
+
               <div className="menu">
                 <Popup
                   trigger={<div className='preventSelect'> <FontAwesomeIcon icon={faList}/> Category:  <FontAwesomeIcon id='categoryIcon' icon={icon} /></div>}
-                  position="right top"
+                  position="right bottom"
                   on="click"
                   closeOnDocumentClick
                   arrow={false}
                 >
                    {(close) => (
-                  <div className="menu">
+                  <div className="menu popup">
                     {categories.map((categ) => (
                       <div className="menu-item preventSelect" onClick={() => {
                         setCategory(categ.name);
